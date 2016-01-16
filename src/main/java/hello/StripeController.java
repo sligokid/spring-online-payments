f7e9b60c394a522173bca0defca9f5509ed8b543
Kieran Mc Gowan <eirwigproject@gmail.com>
2016-01-16 19:53:47 +0000

    Spring MVC online payments demo
    Adding Stripe Checkout

---
+++ src/main/java/hello/StripeController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StripeController {
+
+	@RequestMapping("/stripe")
+	public String stripe() {
+		return "stripe";
+	}
+
+}
